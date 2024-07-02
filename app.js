987e1bf53154013cff0dfad55fde2362b9f6ad61
mondher <110062316+mondher0@users.noreply.github.com>
2024-07-02 12:22:29 +0100

    adding some extra security packages

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 require("express-async-errors");
 
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./db/connect");
+
 const app = express();
 
 const notFoundMiddleware = require("./middlewares/not-found");
@@ -12,6 +17,16 @@ const jobRouter = require("./routes/job");
 
 // middlewares
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
 
 // routes
 app.get("/", (req, res) => {
